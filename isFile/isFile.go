e0067e6cbfc5db3073c4e75b6d4046d75aea60f2
wfercanas <fernando.canas@outlook.com>
2022-02-03 15:46:22 -0500

    add/ check if a file is a regular file

---
+++ isFile/isFile.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	arguments := os.Args
+	if len(arguments) == 1 {
+		fmt.Println("Please give one argument.")
+		return
+	}
+
+	path := arguments[1]
+
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		fmt.Println("Path does not exist!", err)
+		return
+	}
+
+	mode := fileInfo.Mode()
+	fmt.Println("mode():", mode)
+	if mode.IsRegular() {
+		fmt.Println(path, "is a regular file!")
+	}
+}
